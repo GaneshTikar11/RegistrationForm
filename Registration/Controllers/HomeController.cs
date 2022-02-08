a8db553b8948514dbfe4d696f544ea3b655e4c97
Ganesh <ganeshtikar11@gmail.com>
2022-02-08 15:38:32 +0530

    First Commit

---
+++ Registration/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Registration.Models;
+
+namespace Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        RegMVCEntities db = new RegMVCEntities();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(tblRegistration obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                
+                db.tblRegistrations.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+
+        public ActionResult ddlList()
+        {
+            var items = db.tbldistricts.ToList();
+            if (items != null)
+            {
+                ViewBag.data = items;
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ Registration/Models/Register.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Registration.Models
+{
+    public class Register
+    {
+        [Display(Name = "Date", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "DateRequired")]
+        public string Date
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Time", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "TimeRequired")]
+        public string Time
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Name", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "NameRequired")]
+        public string Name
+        {
+            get;
+            set;
+        }
+        [Display(Name = "District", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "DistrictRequired")]
+        public string District
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Taluka", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "TalukaRequired")]
+        public string Taluka
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Village", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "VillageRequired")]
+        public string Village
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Mobile_No", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "MobileNoRequired")]
+        public string Mobile_No
+        {
+            get;
+            set;
+        }
+        [Display(Name = "No_Of_Live_Stock", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "StockRequired")]
+        public string No_Of_Live_Stock
+        {
+            get;
+            set;
+        }
+
+    }
+}
